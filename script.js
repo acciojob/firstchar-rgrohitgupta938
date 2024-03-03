0b61c6aa95dd75ba782e0b1f0bbd3dbe7f3ebb4b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 14:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	for  (let str of text) {
+		if(str!= " "){
+			return str
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
